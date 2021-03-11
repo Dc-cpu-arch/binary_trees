0b6b38368dcf932f44f13b1af257aacafe9e1b35
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-11 18:58:36 +0000

    ISO C90 declarations and code set appart

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,10 +8,12 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftHeight, rightHeight;
+
 	if (!tree)
 		return (0);
-	size_t leftHeight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t rightHeight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	leftHeight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rightHeight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return ((leftHeight > rightHeight) ? leftHeight : rightHeight);
 }
