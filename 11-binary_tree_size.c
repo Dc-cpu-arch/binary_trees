b11fe920c0c0a170bec535370b09a503a9da9fdc
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-11 18:47:21 +0000

    Style Fix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,9 +9,10 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t treeSize;
+
 	if (!tree)
 		return (0);
-	treeSize = binary_tree_size(tree->left)	
+	treeSize = binary_tree_size(tree->left)
 	+ 1 + binary_tree_size(tree->right);
 	return (treeSize);
 }
