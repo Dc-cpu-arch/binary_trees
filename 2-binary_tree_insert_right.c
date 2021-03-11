3328070f06a42e465af20045c68ad8118c17f181
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-11 17:48:50 +0000

    Value handling correction

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,6 +13,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (!parent)
 		return (NULL);
+	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
 	if (parent->right)
 	{
 		new->right = parent->right;
