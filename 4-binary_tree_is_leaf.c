04029940ec6810507bd0486de92053cab5f04c7f
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-11 18:54:40 +0000

    Reconstruction

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,7 +8,12 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node || (!node->left && !node->right))
-		return (1);
+	if (node)
+	{
+		if (!node->left && !node->right)
+			return (1);
+		else
+			return (0);
+	}
 	return (0);
 }
