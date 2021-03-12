829db4de06957836c2e72f67a3c234b4b0385511
Dc-cpu-arch <d.garzong77@gmail.com>
2021-03-12 00:59:56 +0000

    Ternary Conditions added

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	leftHeight = 1 + binary_tree_height(tree->left);
-	rightHeight = 1 + binary_tree_height(tree->right);
+	leftHeight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rightHeight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return ((leftHeight > rightHeight) ? leftHeight : rightHeight);
 }
